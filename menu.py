380036910f28e2f8938da0d5c6f66866a9aafd9e
yixiuGit <xuchun1974@hotmail.com>
2022-09-27 13:35:40 +1000

    clean up

--- menu.py
+++ menu.py
@@ -5,7 +5,6 @@ import pynetbox
 
 file = Path('.').glob('menu.json')
 for i in file:
-    # print(i)
     with open(i) as temfile:
         data = json.load(temfile)
         print(data)
@@ -28,17 +27,15 @@ def main_attr(data):
         rootList.append(k)
     return rootList
 
-secondKeyList = []
-secondList = []
+
+
 pathDic = {}
 filterDic = {}
 filter_list = []
-# temp_filter = {}
+
 def filter_process(data, primay_attr, second_attr, continue_processing):
 
-    # continue_processing = 'yes'
     global filter_list
-    # global temp_filter
     global filterDic
     while continue_processing == 'yes' or continue_processing == 'y':
         continue_processing = input("\n\rAdd data to filter? (y)es to continue, (n)o to finish or (r)eset to clear data and start again: ").lower().strip()
@@ -66,45 +63,6 @@ def filter_process(data, primay_attr, second_attr, continue_processing):
             return filter_process(data, primay_attr, second_attr, "yes")
     else:
         return filter_list
-            # print(filterDic.keys())
-    #         if filter_attr in list(filterDic.keys()) and filter_selector==1:
-    #             filter_processing = input('\n\rThe same filter attribute already exist, it against the rule of creating single filter object. You can select option 2 which is for multiple filter objects.'
-    #                                       '\n\rEnter (n)o to complete build this filter object, (r)eset to go back to previous menu: ').lower().strip()
-    #             if filter_processing not in ('no', 'n', 'reset', 'r'):
-    #                 print("\n\rPlease enter either 'r' or 'n'")
-    #             elif filter_processing == 'no' or filter_processing == 'n':
-    #                 return filterDic
-    #             elif filter_processing == 'reset' or filter_processing == 'r':
-    #                 return filter_process(data, primay_attr, second_attr, filter_selector, "yes")
-    #         elif filter_attr not in list(filterDic.keys()) and filter_selector==1:
-    #             filterDic[filter_attr] = filterData
-    #             return filter_process(data, primay_attr, second_attr, filter_selector, "yes")
-    #         elif filter_attr in list(filterDic.keys()) and filter_selector==2:
-    #             filter_list.append(filterDic)
-    #             temp_filter.update({filter_attr: filterData})
-    #             # temp_filter[filter_attr] = filterData
-    #             filterDic = temp_filter.copy()
-    #             filter_list.append(temp_filter)
-    #
-    #             print(filter_list)
-    #             return filter_process(data, primay_attr, second_attr, filter_selector, "yes")
-    #         elif filter_attr not in list(filterDic.keys()) and filter_selector==2:
-    #             filterDic.update({filter_attr: filterData})
-    #             print(filterDic)
-    #             # filter_list.append(filterDic)
-    #             print(filter_list)
-    #             return filter_process(data, primay_attr, second_attr, filter_selector, "yes")
-    #     elif continue_processing == 'reset' or continue_processing == 'r':
-    #         filterDic.clear()
-    #         return filter_process(data, primay_attr, second_attr,filter_selector, 'yes')
-    # if filter_selector == 1:
-    #     return filterDic
-    # elif filter_selector == 2:
-    #     return filter_list
-
-# filter_selector = int(
-#     input("\n\rPlease select below options to create filter object."
-#           "\n\rEnter 1 to create single filter object, enter 2 to create multiple filter objects: ").strip())
 
 primay_attr = menu(main_attr(data),"main_attr")
 print(primay_attr)
@@ -118,7 +76,7 @@ print(filter_outcome)
 
 
 
-# nb = pynetbox.api("http://127.0.0.1:8000", "ac0045b34d40b36b57c8987512b05f5ba0a27817")
+
 api_attr = f"{primay_attr}.{second_attr}"
 if len(filter_outcome) == 1:
     get_netbox_info.single_object_check(api_attr, filter_outcome[0])
--- netboxGet.py
+++ netboxGet.py
@@ -2,11 +2,6 @@ import pynetbox
 import operator
 import json
 
-
-# allprefixes = operator.attrgetter(api_attr)(nb).filter(**dic_filter)
-# # allprefixes = operator.attrgetter(api_attr)(nb).filter(**{obj_filter: filter_data, obj_filter1: filter_data1 })
-# print(list(allprefixes))
-
 class get_netbox_info:
 
     def __init__(self, netbox_ip, netbox_token):
@@ -48,51 +43,7 @@ class get_netbox_info:
             # del prefix_dic['children']
             # del prefix_dic['_depth']
             print(prefix_dic)
-            # with open ('output.json', 'w') as outfile:
-            #     json.dump(prefix_dic, outfile)
+
         else:
             print("object does not exist")
-        # return  list(return_object)
 
-
-
-#
-# if __name__ == '__main__':
-#     api_attr = "ipam.prefixes"
-#     obj_filter = "prefix"
-#     filter_data = ["10.0.0.0/28"]
-#     obj_filter1 = "status"
-#     filter_data1 = "container"
-#     nb = pynetbox.api("http://127.0.0.1:8000", "ac0045b34d40b36b57c8987512b05f5ba0a27817")
-#     # dic_filter = {obj_filter: filter_data, obj_filter1: filter_data1}
-#     dic_filter = {obj_filter: filter_data}
-#     dic_filter = [{obj_filter: filter_data, obj_filter1: filter_data1}, {obj_filter: ["10.0.0.0/28"], obj_filter1: 'active'}]
-
-
-
-    ### for single enquiry
-    ### This is for try to search for 1 object, all the search query is in 1 dictionary, the more searching criteria you have
-    ### the more percise return you will have .
-    ### It may return more than 1 object if your search query is too broad.
-    ### For example, you may have multiple ip prefixes they have same ip and mask, but different status
-    # response = get_netbox_info.single_object_check(api_attr,dic_filter)
-            # print(list(response))
-            # if len(list(response)) > 1:
-            #     print('2')
-            #     for obj in list(response):
-            #         print(obj)
-            #         print(dict(obj)['status'])
-            # elif len(response) == 1:
-            #     print('1')
-            #     print(dict(list(response)[0])['status'])
-            # else:
-            #     print("object does not exist")
-
-
-
-    #multiple
-    # obj_notexist, obj_exist = get_netbox_info.multi_objects_check(api_attr, dic_filter)
-    # print(obj_notexist)
-    # print(obj_exist)
-    # for obj in list(obj_exist):
-    #     print(dict(list(obj)[0])['status'])
